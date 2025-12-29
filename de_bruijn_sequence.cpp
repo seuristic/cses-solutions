7b23240b7e9ac82efc51bad1efcf9d24bce96ca4
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2025-12-29 15:53:53 +0530

    refactor: implement De Bruijn Sequence using backtracking approach

--- de_bruijn_sequence.cpp
+++ de_bruijn_sequence.cpp
@@ -19,46 +19,88 @@ using ll = long long;
 #define endl '\n'
 #define all(x) begin(x), end(x)
 
+// int n;
+// vector<vector<int>> adj;
+// vector<int> path;
+
+// APPROACH 1: bitmanip + dfs (Hierholzer's algo) => eulerian circuit
+// void dfs(int u) {
+//     while (!adj[u].empty()) {
+//         int v = adj[u].back();
+//         adj[u].pop_back();
+//         dfs(v);
+//     }
+//     path.push_back(u & 1);
+// }
+
+// void solve() {
+//     cin >> n;
+
+//     if (n == 1) {
+//         cout << "01" << endl;
+//         return;
+//     }
+
+//     adj.assign((1 << (n - 1)), {});
+//     path.clear();
+
+//     for (int i = 0; i < (1 << (n - 1)); ++i) {
+//         int j = (i << 1) % (1 << (n - 1));
+//         adj[i].push_back(j);
+//         adj[i].push_back(j | 1);
+//     }
+
+//     dfs(0);
+
+//     for (int i = 0; i < n - 2; ++i) {
+//         path.push_back(0);
+//     }
+
+//     for (int c : path) {
+//         cout << char(c + '0');
+//     }
+//     cout << endl;
+// }
+
+// APPROACH 2: backtracking
 int n;
-vector<vector<int>> adj;
+vector<bool> visited;
 vector<int> path;
 
-void dfs(int u) {
-    while (!adj[u].empty()) {
-        int v = adj[u].back();
-        adj[u].pop_back();
-        dfs(v);
+void search(int pos, int part, int n) {
+    if (pos == (1 << n) + n - 1) {
+        for (int p : path) {
+            cout << p;
+        }
+        cout << endl;
+        exit(0);
     }
-    path.push_back(u & 1);
+
+    int next_part0 = ((part << 1) & ((1 << n) - 1));
+    if (!visited[next_part0]) {
+        visited[next_part0] = true;
+        path[pos] = 0;
+        search(pos + 1, next_part0, n);
+        visited[next_part0] = false;
+    }
+
+    int next_part1 = (((part << 1) & ((1 << n) - 1)) | 1);
+    if (!visited[next_part1]) {
+        visited[next_part1] = true;
+        path[pos] = 1;
+        search(pos + 1, next_part1, n);
+        visited[next_part1] = false;
+    }
 }
 
 void solve() {
     cin >> n;
 
-    if (n == 1) {
-        cout << "01" << endl;
-        return;
-    }
+    visited.assign(1 << n, false);
+    path.assign((1 << n) + n - 1, 0);
 
-    adj.assign((1 << (n - 1)), {});
-    path.clear();
-
-    for (int i = 0; i < (1 << (n - 1)); ++i) {
-        int j = (i << 1) % (1 << (n - 1));
-        adj[i].push_back(j);
-        adj[i].push_back(j | 1);
-    }
-
-    dfs(0);
-
-    for (int i = 0; i < n - 2; ++i) {
-        path.push_back(0);
-    }
-
-    for (int c : path) {
-        cout << char(c + '0');
-    }
-    cout << endl;
+    visited[0] = true;
+    search(n, 0, n);
 }
 
 int main() {
