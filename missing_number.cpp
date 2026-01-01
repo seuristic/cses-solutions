915d95ffa9fc3f91340e9a330efb2a5ff48dfb86
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2026-01-01 19:09:34 +0530

    refactor: reorganize missing number solution for clarity and efficiency

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,27 +1,49 @@
-#include <bits/stdc++.h>
-#define endl '\n'
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+#define F first
+#define S second
+#define PB push_back
+#define ALL(x) begin(x), end(x)
+#define SIZE(x) (int)(x).size()
+#define ENDL '\n'
 
-  int n;
-  cin >> n;
-  vector<int> a(n, -1);
-  for (int i = 0; i < n - 1; ++i) {
-    int x;
-    cin >> x;
-    a[x - 1] = 1;
-  }
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
-  for (int i = 0; i < n; ++i) {
-    if (a[i] == -1) {
-      cout << i + 1 << endl;
-      break;
+void solve() {
+    int n;
+    cin >> n;
+    ll sum = 0;
+    for (int i = 1; i < n; ++i) {
+        int x;
+        cin >> x;
+        sum += x;
     }
-  }
 
-  return 0;
+    ll missing = 1LL * n * (n + 1) / 2 - sum;
+    cout << missing << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
 }
